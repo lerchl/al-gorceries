b49d5fee61ba0e4192395a6caa348975dee33794
Nico Lerchl <nico@lerchl.one>
2023-09-01 18:19:26 +0200

    scrollable dish list

--- frontend/src/routes/index/DishesOfDishList.js
+++ frontend/src/routes/index/DishesOfDishList.js
@@ -4,13 +4,13 @@ import { DishListDish } from "./DishListDish";
 export const DishesOfDishList = ({ dishListDishes, setDishListDishes }) => {
 
     return (
-        <>
+        <div className="dish-list-wrapper">
             {
                 dishListDishes.map(dishListDish =>
                         <DishListDish key={dishListDish.id}
                                       dishListDish={dishListDish}
                                       setDishListDishes={setDishListDishes} />)
             }
-        </>
+        </div>
     );
 }
